4014ba53199f8bea513f72f6af8840b7b9f65249
Erik van Zijst <erik.van.zijst@gmail.com>
2012-09-27 19:08:53 -0700

    Removed unused qrcode.

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 DEPLOY_DIR=`mktemp -d -t djangocon-XXXXXX`
 
 echo "Cloning evzijst.bitbucket.org into $DEPLOY_DIR..."
-( cd $DEPLOY_DIR && git clone https://evzijst@bitbucket.org/evzijst/evzijst.bitbucket.org.git evzijst )
+( cd $DEPLOY_DIR && git clone git@bitbucket.org:evzijst/evzijst.bitbucket.org.git evzijst )
 
 echo "Merging content into site repo..."
 rsync -r --delete slides/* $DEPLOY_DIR/evzijst/pycon.in/
