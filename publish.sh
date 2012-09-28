bfe7ea1067124ff3422eab72ac214a21435878f6
Erik van Zijst <erik.van.zijst@gmail.com>
2012-09-27 19:06:37 -0700

    Updated for PyCon India 2012.

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 DEPLOY_DIR=`mktemp -d -t djangocon-XXXXXX`
 
 echo "Cloning evzijst.bitbucket.org into $DEPLOY_DIR..."
-( cd $DEPLOY_DIR && git clone git@bitbucket.org:evzijst/evzijst.bitbucket.org.git evzijst )
+( cd $DEPLOY_DIR && git clone https://evzijst@bitbucket.org/evzijst/evzijst.bitbucket.org.git evzijst )
 
 echo "Merging content into site repo..."
-rsync -r --delete slides/* $DEPLOY_DIR/evzijst/djangocon.eu/
+rsync -r --delete slides/* $DEPLOY_DIR/evzijst/pycon.in/
 
 ( cd $DEPLOY_DIR/evzijst && \
     git add --all && \
